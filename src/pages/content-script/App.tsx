511428b73b1a74f243b3318b5d0def2fcae0e1fd
sunxinyu <sunxinyu@yucekj.com>
2023-02-19 20:07:01 +0800

    Fix style and icon

--- src/pages/content-script/App.tsx
+++ src/pages/content-script/App.tsx
@@ -108,7 +108,12 @@ function Root() {
           Pending Reports
         </Text>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body
+        css={{
+          maxHeight: '310px',
+          overflow: 'auto',
+        }}
+      >
         {rows.length === 0 ? (
           <div
             style={{
@@ -124,7 +129,7 @@ function Root() {
         ) : (
           <Table
             css={{
-              maxHeight: '200px',
+              height: '300px',
               minWidth: '100%',
             }}
           >
@@ -147,6 +152,13 @@ function Root() {
                         console.log('remove', item.userId);
                         removeUserId(item.userId);
                       }}
+                      style={{
+                        border: '1px solid #D0D5DD',
+                        boxShadow: '0px 1px 2px rgba(16, 24, 40, 0.05)',
+                        borderRadius: '100px',
+                        width: '94px',
+                        height: '30px',
+                      }}
                     >
                       Remove
                     </Button>
@@ -157,18 +169,37 @@ function Root() {
           </Table>
         )}
       </Modal.Body>
-      <Modal.Footer>
-        <Button auto flat color="error" onPress={closeHandler}>
-          Cancel
-        </Button>
+      <Modal.Footer justify="center">
         <Button
           auto
           onPress={() => {
             window.open(config.dappUrl);
           }}
+          style={{
+            width: '253px',
+            height: '48px',
+            background: 'linear-gradient(90deg, #334FB8 0%, #1D81BE 100%)',
+            boxShadow: '0px 1px 2px rgba(16, 24, 40, 0.05)',
+            borderRadius: '100px',
+            color: '#fff',
+          }}
         >
           Submit batch to blockchain
         </Button>
+
+        <div
+          style={{
+            fontWeight: 400,
+            fontSize: '14px',
+            lineHeight: '20px',
+            color: '#666F85',
+            textAlign: 'center',
+            marginTop: '10px',
+          }}
+        >
+          You can always find the entire list of pending reports on the official
+          website: <a href="https://mobymask.com">mobymask.com</a>
+        </div>
       </Modal.Footer>
     </Modal>
   );
--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -101,9 +101,8 @@ function Root() {
   return (
     <div
       style={{
-        width: '300px',
-        height: '400px',
-        padding: '20px',
+        width: '360px',
+        padding: '10px',
       }}
     >
       <Text id="modal-title" size={18}>
@@ -123,47 +122,90 @@ function Root() {
           No Records
         </div>
       ) : (
-        <Table
-          css={{
-            maxHeight: '200px',
+        <div
+          style={{
+            height: '310px',
+            overflow: 'auto',
             minWidth: '100%',
           }}
         >
-          <Table.Header columns={columns}>
-            {(column) => (
-              <Table.Column key={column.key}>{column.label}</Table.Column>
-            )}
-          </Table.Header>
-          <Table.Body items={rows}>
-            {(item) => (
-              <Table.Row key={item.key}>
-                <Table.Cell>@{item.userId}</Table.Cell>
-                <Table.Cell>
-                  <Link
-                    href="#"
-                    onClick={() => {
-                      removeUserId(item.userId);
-                    }}
-                  >
-                    Remove
-                  </Link>
-                </Table.Cell>
-              </Table.Row>
-            )}
-          </Table.Body>
-        </Table>
+          <Table>
+            <Table.Header columns={columns}>
+              {(column) => (
+                <Table.Column key={column.key}>{column.label}</Table.Column>
+              )}
+            </Table.Header>
+            <Table.Body items={rows}>
+              {(item) => (
+                <Table.Row key={item.key}>
+                  <Table.Cell>@{item.userId}</Table.Cell>
+                  <Table.Cell>
+                    <Button
+                      bordered
+                      color="primary"
+                      auto
+                      size="xs"
+                      onClick={() => {
+                        console.log('remove', item.userId);
+                        removeUserId(item.userId);
+                      }}
+                      style={{
+                        border: '1px solid #D0D5DD',
+                        boxShadow: '0px 1px 2px rgba(16, 24, 40, 0.05)',
+                        borderRadius: '100px',
+                        width: '94px',
+                        height: '30px',
+                      }}
+                    >
+                      Remove
+                    </Button>
+                  </Table.Cell>
+                </Table.Row>
+              )}
+            </Table.Body>
+          </Table>
+        </div>
       )}
-      <Button
-        auto
-        onPress={() => {
-          window.open(config.dappUrl);
-        }}
+      <div
         style={{
+          display: 'flex',
+          justifyContent: 'center',
           marginTop: '20px',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
       >
-        Submit batch to blockchain
-      </Button>
+        <Button
+          auto
+          onPress={() => {
+            window.open(config.dappUrl);
+          }}
+          style={{
+            width: '253px',
+            height: '48px',
+            background: 'linear-gradient(90deg, #334FB8 0%, #1D81BE 100%)',
+            boxShadow: '0px 1px 2px rgba(16, 24, 40, 0.05)',
+            borderRadius: '100px',
+            color: '#fff',
+          }}
+        >
+          Submit batch to blockchain
+        </Button>
+
+        <div
+          style={{
+            fontWeight: 400,
+            fontSize: '14px',
+            lineHeight: '20px',
+            color: '#666F85',
+            textAlign: 'center',
+            marginTop: '10px',
+          }}
+        >
+          You can always find the entire list of pending reports on the official
+          website: <a href="https://mobymask.com">mobymask.com</a>
+        </div>
+      </div>
     </div>
   );
 }
