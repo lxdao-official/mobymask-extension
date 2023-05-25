98ed4bf9c8932bdfe3922f72cf9aec2b35509ac1
Bruce Xu <bruce.xu@lxdao.io>
2023-05-25 15:13:56 +1200

    Remove unused code

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,10 +1,1 @@
 export {};
-
-// chrome.storage.local.get(['isFirst'], async function (data) {
-//   if (!data.isFirst) {
-//     chrome.tabs.create({
-//       url: 'dashboard/index.html',
-//     });
-//     chrome.storage.local.set({ isFirst: true });
-//   }
-// });
